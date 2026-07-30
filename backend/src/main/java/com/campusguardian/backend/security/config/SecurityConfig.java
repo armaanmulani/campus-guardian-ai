fcf485860dc9c285aecbfdb25eccb52271fbf040
armaanmulani <armaanmulani941@gmail.com>
2026-07-30 22:46:49 +0530

    Configure spring security

--- backend/src/main/java/com/campusguardian/backend/security/config/SecurityConfig.java
+++ backend/src/main/java/com/campusguardian/backend/security/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.campusguardian.backend.security;
+package com.campusguardian.backend.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
