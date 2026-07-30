273c466b610340bc791291334f4f043e920429c7
armaanmulani <armaanmulani941@gmail.com>
2026-07-30 18:48:35 +0530

    Connection to PostgreSQL

--- backend/src/main/java/com/campusguardian/backend/BackendApplication.java
+++ backend/src/main/java/com/campusguardian/backend/BackendApplication.java
@@ -1,4 +1,4 @@
-package com.campusguardianai.backend;
+package com.campusguardian.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
